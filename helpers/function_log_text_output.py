3c489214fb351e6605719a060d01631e14a4889d
Me <58314686+ashish911in@users.noreply.github.com>
2023-06-23 13:44:05 +1000

    small changes

--- helpers/function_log_text_output.py
+++ helpers/function_log_text_output.py
@@ -1,4 +1,4 @@
-def call_function(function_name,args):
+def call_function(function_name,*args):
     print(f'{function_name.__name__}({type(args[0])},{type(args[1])}) fn started')
     function_name(args[0],args[1])
     print(f'{function_name.__name__}() fn  ended')
@@ -9,4 +9,4 @@ def sample_function(arg1, arg2):
     return True
 
 if __name__ == '__main__':
-    call_function(sample_function,[1,2])
+    call_function(sample_function,1,2)
