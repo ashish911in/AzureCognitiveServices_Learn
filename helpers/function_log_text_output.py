bb67376c2687ce3ee335402aedcafa6517f0f3b0
Me <58314686+ashish911in@users.noreply.github.com>
2023-06-27 13:43:43 +1000

    common fn start-stop msg code(not implemented yet)

--- helpers/function_log_text_output.py
+++ helpers/function_log_text_output.py
@@ -1,12 +1,20 @@
-def call_function(function_name,*args):
-    print(f'{function_name.__name__}({type(args[0])},{type(args[1])}) fn started')
-    function_name(args[0],args[1])
+def deconstruct_arguments(**kwargs):
+    return_entity = ''
+    for k,v in kwargs.items():
+        return_entity += str(type(v))
+    return return_entity
+    
+def call_function(function_name,**kwargs):
+    print(f'{function_name.__name__}({deconstruct_arguments(**kwargs)}) fn started')
+    function_name(**kwargs)
     print(f'{function_name.__name__}() fn  ended')
 
-def sample_function(arg1, arg2):
+def add_function(**kwargs):
     print("Welcome")
-    print(arg1+arg2)
+    print(kwargs['no1']+kwargs['no2'])
     return True
 
 if __name__ == '__main__':
-    call_function(sample_function,1,2)
+    call_function(add_function,no1=1,no2=2)
+
+    call_function(add_function,no1=45,no2=20)
